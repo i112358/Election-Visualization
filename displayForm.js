7100e2054e58002d25b20bc506dba897feea0244
Rachel Chang <juc62@pitt.edu>
2017-05-06 13:04:46 -0400

    Fixed the bottom form to display data in json format

--- displayForm.js
+++ displayForm.js
@@ -1,11 +1,9 @@
 function displayForm(){
-  d3.select("form").on("submit", function(){
-    var jsonForm = {};
-    var form = d3.select("#simpleForm");
-    jsonForm["state"] = form["state"].value;
-    jsonForm["name"] = form["name"].value;
-    jsonForm["votes"] = form["votes"].value;
-    var x = JSON.stringify(jsonForm);
-    return d3.select("pre").text(x);
-  });
+  var jsonForm = {};
+    var form = document.getElementById("simpleForm");
+    jsonForm["state"] = form.elements["state"].value;
+    jsonForm["name"] = form.elements["name"].value;
+    jsonForm["votes"] = form.elements["votes"].value;
+    var x =  JSON.stringify(jsonForm);
+    d3.select(".debug").text(x);
 }
